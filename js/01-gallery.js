c8b1a84dd4dbed22bda79fc3477fa500858e5035
MaksymilianBilski <biger9@o2.pl>
2022-09-29 20:39:27 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,8 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 const images = gallery.childNodes;
-console.log(galleryItems);
+const items = galleryItems.map((a) => a.childNodes);
+console.log(items);
 
 for (const item of galleryItems) {
   const img = document.createElement("img");
@@ -14,30 +15,43 @@ for (const item of galleryItems) {
   gallery.append(img);
 }
 const img = document.querySelectorAll(".gallery img");
+console.log(gallery);
+console.log(images[3]);
 
 gallery.addEventListener("click", (event) => {
   if (event.target.nodeName !== "IMG") {
+    console.log("test");
     return;
   }
-  console.log("to jest targetto: " + event.target.src);
-  event.target.removeAttribute("src");
-  event.target.setAttribute(
-    "data-source",
-    `${(event.currentTarget.dataset = "source")}`
-  );
-  event.target.style.width = "1278px";
-  event.target.style.height = "853px";
-  event.target.style.position = "absolute";
-  gallery.addEventListener("click", (event) => {
-    if (event.target.nodeName !== "IMG") {
-      return;
-    }
-    event.target.removeAttribute("src");
-    event.target.setAttribute("src", event.currentTarget.src);
-    event.target.style.position = "static";
-    event.target.style.width = "100%";
-    event.target.style.height = "100%";
-  });
+  for (const img of images) {
+    console.log(event.target.src);
+    event.target.src;
+    console.log(galleryItems.indexOf(event.target.src));
+    console.log(items);
+    return;
+  }
 });
+
+// gallery.addEventListener("click", (event) => {
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   console.log("to jest targetto: " + event.target.src);
+//   event.target.removeAttribute("src");
+//   event.target.setAttribute("data-source", `${event.currentTarget.src}`);
+//   event.target.style.width = "1278px";
+//   event.target.style.height = "853px";
+//   event.target.style.position = "absolute";
+//   gallery.addEventListener("click", (event) => {
+//     if (event.target.nodeName !== "IMG") {
+//       return;
+//     }
+//     event.target.removeAttribute("src");
+//     event.target.setAttribute("src", event.currentTarget.src);
+//     event.target.style.position = "static";
+//     event.target.style.width = "100%";
+//     event.target.style.height = "100%";
+//   });
+// });
 
 console.log(img);
