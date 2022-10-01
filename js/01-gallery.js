a3c48814882ff1481c160f0368b5093f8ac7ef85
MaksymilianBilski <biger9@o2.pl>
2022-10-01 13:54:20 +0200

    koniec

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,39 +2,112 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-const newGallery = galleryItems
-  .map(
-    ({
-      preview,
-      original,
-      description,
-    }) => `<a class="gallery__link" href="${original}">
+galleryItems.map(({ preview, original, description }) =>
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
         <img
           class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
         />
-      </a>`
+      </a>
+    </div>`
   )
-  .join("");
-gallery.innerHTML = newGallery;
-const child = document.querySelector(".gallery__link");
+);
 
 const onClick = (event) => {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  const evt = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  console.log(event.target);
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} alt=${event.target.description}></img>`
   );
-  evt.show();
-  const escape = (e) => {
-    if (e.code === "Escape") {
-      evt.close();
+  instance.show();
+  const onEscape = (key) => {
+    if (key.code === "Escape") {
+      instance.close();
     }
   };
-  window.addEventListener("keydown", escape);
+  window.addEventListener("keydown", onEscape);
 };
+
 gallery.addEventListener("click", onClick);
+
+//2 metoda
+
+// const gallery = document.querySelector(".gallery");
+// const newGallery = galleryItems
+//   .map(
+//     ({
+//       preview,
+//       original,
+//       description,
+//     }) => `<a class="gallery__link" href="${original}">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           data-source="${original}"
+//           alt="${description}"
+//         />
+//       </a>`
+//   )
+//   .join("");
+// gallery.innerHTML = newGallery;
+// const child = document.querySelector(".gallery__link");
+
+// const onClick = (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const evt = basicLightbox.create(
+//     `<img src="${event.target.dataset.source}" width="800" height="600">`
+//   );
+//   evt.show();
+//   const escape = (e) => {
+//     if (e.code === "Escape") {
+//       evt.close();
+//     }
+//   };
+//   window.addEventListener("keydown", escape);
+// };
+// gallery.addEventListener("click", onClick);
+
+// 3 metoda(niedokonczona)
+
+// const gallery = document.querySelector(".gallery");
+// for (const item of galleryItems) {
+//   const img = gallery.insertAdjacentHTML(
+//     "beforeend",
+// `<div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src=${item.preview}
+//       data-source=${item.original}
+//       alt=${item.description}
+//     />
+//   </a>
+// </div>`
+//   );
+// }
+// const image = document.querySelectorAll(".gallery__image");
+
+// const onClick = (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   event.target.removeAttribute("src");
+//   event.target.setAttribute("src", event.target.dataset.source);
+
+//   console.log(event.target.dataset.source);
+//   console.log(event.target.dataset);
+// };
+
+// gallery.addEventListener("click", onClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const child = gallery.childNodes;
 
-console.log(galleryItems);
+for (const item of galleryItems) {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `
+      <a class="gallery__item" href=${item.original}>
+        <img
+          class="gallery__image"
+          src=${item.preview}
+          alt=${item.description}
+        />
+      </a>`
+  );
+}
+const lightbox = () => {
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    canimationSpeed: 125,
+    captionPosition: "outside",
+    fadeSpeed: 650,
+  });
+};
+
+gallery.addEventListener("click", lightbox());
