c520c635711c3a88809126a88f59032a8e6145fe
MaksymilianBilski <biger9@o2.pl>
2022-11-05 20:13:31 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,42 +1,43 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-galleryItems.map(({ preview, original, description }) =>
-  gallery.insertAdjacentHTML(
-    "beforeend",
-    `<div class="gallery__item">
-      <a class="gallery__link" href="large-image.jpg">
-        <img
-          class="gallery__image"
-          src=${preview}
-          data-source=${original}
-          alt=${description}
-        />
-      </a>
-    </div>`
-  )
-);
+// const gallery = document.querySelector(".gallery");
+// galleryItems.map(({ preview, original, description }) =>
+//   gallery.insertAdjacentHTML(
+//     "beforeend",
+//     `<div class="gallery__item">
+//       <a class="gallery__link" href="large-image.jpg">
+//         <img
+//           class="gallery__image"
+//           src=${preview}
+//           data-source=${original}
+//           alt=${description}
+//         />
+//       </a>
+//     </div>`
+//   )
+// );
 
-const onClick = (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  console.log(event.target);
-  const instance = basicLightbox.create(
-    `<img src=${event.target.dataset.source} alt=${event.target.description}></img>`
-  );
-  instance.show();
-  const onEscape = (key) => {
-    if (key.code === "Escape") {
-      instance.close();
-    }
-  };
-  window.addEventListener("keydown", onEscape);
-};
+// const onClick = (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   console.log(event.target);
+//   const instance = basicLightbox.create(
+//     `<img src=${event.target.dataset.source} alt=${event.target.description}></img>`
+//   );
+//   instance.show();
+//   const onEscape = (key) => {
+//     if (key.code === "Escape") {
+//       instance.close();
+//     }
+//   };
+//   window.addEventListener("keydown", onEscape);
+// };
 
-gallery.addEventListener("click", onClick);
+// gallery.addEventListener("click", onClick);
 
 //2 metoda
 
@@ -78,7 +79,42 @@ gallery.addEventListener("click", onClick);
 // };
 // gallery.addEventListener("click", onClick);
 
-// 3 metoda(niedokonczona)
+//3 metoda
+const gallery = document.querySelector(".gallery");
+let instance;
+
+for (let i = 0; i <= galleryItems.length - 1; i++) {
+  gallery.insertAdjacentHTML(
+    "afterbegin",
+    `<img src="${galleryItems[i].preview}" data-source="${galleryItems[i].original}" style="width:100%; height:100%;"alt="${galleryItems[i].description}"></img>`
+  );
+}
+
+function showItem(e) {
+  e.preventDefault;
+  if (e.target.parentNode !== gallery) {
+    return;
+  } else {
+    for (let i = 0; i <= galleryItems.length - 1; i++) {
+      instance = basicLightbox.create(
+        `<img src="${e.target.dataset.source}"></img>`
+      );
+    }
+  }
+  instance.show();
+}
+
+function closeItem(e) {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+  return;
+}
+
+gallery.addEventListener("click", showItem);
+window.addEventListener("keydown", closeItem);
+
+// 4 metoda(niedokonczona)
 
 // const gallery = document.querySelector(".gallery");
 // for (const item of galleryItems) {
